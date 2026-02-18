c90a95dd890e2998d1a84c948d8837909cfba009
Felipe Rivas <79724739+feliperivasdev@users.noreply.github.com>
2026-02-18 12:03:57 -0500

    Feat: Add Crud

--- controllers/unidades_medidaController.js
+++ controllers/unidades_medidaController.js
@@ -23,4 +23,33 @@ module.exports = {
       })
       .catch((error) => res.status(400).send(error));
   },
+  add(req, res) {
+    return unidades_medida
+      .create({
+        codigo: req.body.codigo,
+        
+      })
+      .then((unidades_medida) => res.status(201).send(unidades_medida))
+      .catch((error) => res.status(400).send(error));
+  },
+  update(req, res) {
+ return unidades_medida
+ .findByPk(req.params.id)
+ .then(unidades_medida => {
+ if (!unidades_medida) {
+ return res.status(404).send({
+ message: 'unidades_medida Not Found',
+ });
+ }
+ return unidades_medida
+ .update({
+ codigo: req.body.codigo || unidades_medida.codigo,
+ nombre: req.body.nombre || unidades_medida.nombre,
+ 
+ })
+ .then(() => res.status(200).send(unidades_medida))
+ .catch((error) => res.status(400).send(error));
+ })
+ .catch((error) => res.status(400).send(error));
+ },
 };
