f1ba1fdc4d5a05f3d1b02d2f534b895ec4cb7861
Aslly Zuñiga <asllyz2610@gmail.com>
2026-02-18 08:51:07 -0500

    update routes

--- routes/tipos_documentos.js
+++
@@ -1,5 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const tipos_documentosController = require('../controllers').tipos_documentosController;
-router.get('/', tipos_documentosController.list);
-module.exports = router;
