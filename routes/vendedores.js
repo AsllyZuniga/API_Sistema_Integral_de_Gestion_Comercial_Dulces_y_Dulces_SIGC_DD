60825c2a63bfae1ed8c250d61132b34f51f8c063
Felipe Rivas <79724739+feliperivasdev@users.noreply.github.com>
2026-02-19 08:21:17 -0500

    Fix: Update routes

--- routes/vendedores.js
+++ routes/vendedores.js
@@ -3,4 +3,6 @@ var router = express.Router();
 const vendedoresController = require('../controllers').vendedoresController;
 router.get('/', vendedoresController.list);
 router.get('/:id', vendedoresController.getById);
+router.post('/',vendedoresController.add);
+router.put('/:id', vendedoresController.update);
 module.exports = router;
