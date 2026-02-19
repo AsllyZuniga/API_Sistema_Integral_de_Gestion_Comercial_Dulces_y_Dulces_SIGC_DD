ecf03e0adcd5942c7ee0f8dd0086f90a5502ae7d
Felipe Rivas <79724739+feliperivasdev@users.noreply.github.com>
2026-02-19 08:21:17 -0500

    Fix: Update routes

--- routes/unidades_medida.js
+++ routes/unidades_medida.js
@@ -3,4 +3,6 @@ var router = express.Router();
 const unidades_medidaController = require('../controllers').unidades_medidaController;
 router.get('/', unidades_medidaController.list);
 router.get('/:id', unidades_medidaController.getById);
+router.post('/', unidades_medidaController.add);
+router.put('/:id', unidades_medidaController.update);
 module.exports = router;
--- routes/ventas.js
+++ routes/ventas.js
@@ -3,4 +3,6 @@ var router = express.Router();
 const ventasController = require('../controllers').ventasController;
 router.get('/', ventasController.list);
 router.get('/:id', ventasController.getById);
+router.post('/', ventasController.add);
+router.put('/:id', ventasController.update);
 module.exports = router;
