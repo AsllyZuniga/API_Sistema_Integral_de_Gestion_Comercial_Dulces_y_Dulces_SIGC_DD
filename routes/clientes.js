8e2a024e2f2d00af529a95593b582def91b01cf4
Felipe Rivas <79724739+feliperivasdev@users.noreply.github.com>
2026-03-05 08:38:29 -0500

    new beginnings

--- routes/clientes.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-const clientesController = require("../controllers").clientesController;
-router.get("/", clientesController.list);
-router.get("/:id", clientesController.getById);
-router.post('/', clientesController.add);
-router.put('/:id', clientesController.update);
-
-module.exports = router;
