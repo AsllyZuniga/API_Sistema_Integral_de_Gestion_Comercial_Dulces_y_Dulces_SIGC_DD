77af28d16837a156edd690de2edd5f4e64962adc
Felipe Rivas <79724739+feliperivasdev@users.noreply.github.com>
2026-03-05 08:38:29 -0500

    new beginnings

--- app.js
+++ app.js
@@ -5,21 +5,11 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var categoriasRouter = require("./routes/categorias");
-var clientesRouter = require("./routes/clientes");
-var cuotas_vendedoresRouter = require("./routes/cuotas_vendedores");
-var productosRouter = require("./routes/productos");
-var staging_ventasRouter = require("./routes/staging_ventas");
-var tipos_documentoRouter = require("./routes/tipos_documento");
-var unidades_medidaRouter = require("./routes/unidades_medida");
-var vendedoresRouter = require("./routes/vendedores");
-var ventasRouter = require("./routes/ventas");
-var ventas_detalleRouter = require("./routes/ventas_detalle");
-var cumplimientoMesRouter = require("./routes/cumplimentoMes");
 
-var importRouter = require("./routes/import.routes");
-var authRouter = require("./routes/auth.routes");
+
+
+
+
 
 
 var cors = require('cors');
@@ -38,21 +28,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/categorias", categoriasRouter);
-app.use("/clientes", clientesRouter);
-app.use("/cuotas_vendedores", cuotas_vendedoresRouter);
-app.use("/productos", productosRouter);
-app.use("/staging_ventas", staging_ventasRouter);
-app.use("/tipos_documento", tipos_documentoRouter);
-app.use("/unidades_medida", unidades_medidaRouter);
-app.use("/vendedores", vendedoresRouter);
-app.use("/ventas", ventasRouter);
-app.use("/ventas_detalle", ventas_detalleRouter);
-app.use("/mes", cumplimientoMesRouter);
 
-app.use("/import", importRouter);
-app.use("/api/auth", authRouter);
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/unidades_medida.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const unidades_medidaController = require('../controllers').unidades_medidaController;
-router.get('/', unidades_medidaController.list);
-router.get('/:id', unidades_medidaController.getById);
-router.post('/', unidades_medidaController.add);
-router.put('/:id', unidades_medidaController.update);
-module.exports = router;
