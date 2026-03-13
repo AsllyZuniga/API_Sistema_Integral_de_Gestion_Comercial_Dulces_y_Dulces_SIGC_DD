96c058916de8d0f330199a7531be34ba2def9b53
Felipe Rivas <79724739+feliperivasdev@users.noreply.github.com>
2026-03-13 14:16:53 -0500

    auth vendedores

---
+++ routes/authRouter.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+const authController = require('../controllers').authController;
+
+router.post('/login', authController.login);
+router.post('/register', authController.register);
+router.post('/register/bulk', authController.registerBulk);
+
+module.exports = router;
