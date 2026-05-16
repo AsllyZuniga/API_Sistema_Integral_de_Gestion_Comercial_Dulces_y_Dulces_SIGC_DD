9c9b42f65b9acf7b040d030484164b0c53e644be
David Felipe Gustin Rivas <79724739+feliperivasdev@users.noreply.github.com>
2026-05-16 10:24:37 -0500

    Busqueda por ciudad

--- routes/cumplimientoMesRouter.js
+++ routes/cumplimientoMesRouter.js
@@ -3,16 +3,22 @@ var router = express.Router();
 const cumplimientoMesController = require('../controllers').cumplimientoMesController;
 const { requireAuthJWT } = require('../middlewares/authJwtMiddleware');
 
+// Rutas específicas sin parámetros (PRIMERO)
 router.get('/front/me', requireAuthJWT, cumplimientoMesController.listFrontMe);
 router.get('/front', cumplimientoMesController.listFront);
+router.get('/ciudades-global', cumplimientoMesController.getCiudadesGlobal);
 router.get('/lineas', cumplimientoMesController.getLineas);
-router.get('/', cumplimientoMesController.list);
-router.get('/vendedor/:codigoVendedor', cumplimientoMesController.getByVendedor);
+
+// Rutas con parámetros anidados
 router.get('/vendedor/:codigoVendedor/linea/:codigoLinea', cumplimientoMesController.getLineaEspecificaPorVendedor);
 router.get('/vendedor/:codigoVendedor/lineas', cumplimientoMesController.getLineasPorVendedor);
 router.get('/vendedor/:codigoVendedor/ciudades', cumplimientoMesController.getCiudadesPorVendedor);
 router.get('/vendedor/:codigoVendedor/ciudad/:idCiudad', cumplimientoMesController.getCiudadEspecificaPorVendedor);
 router.get('/vendedor/:codigoVendedor/productos', cumplimientoMesController.getProductosPorVendedor);
+router.get('/vendedor/:codigoVendedor', cumplimientoMesController.getByVendedor);
+
+// Rutas genéricas (AL FINAL)
+router.get('/', cumplimientoMesController.list);
 router.get('/:codigo', cumplimientoMesController.getByCodigo);
 
 module.exports = router;
